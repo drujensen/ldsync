fd55dbd27b3de324c8f89e0b21bec0ce9fe738d9
Dru Jensen <djensen@happymoney.com>
2022-03-14 15:36:58 -0700

    refactor: move config to root

--- spec/commands/init_spec.cr
+++ spec/commands/init_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 describe Init do
   it "should create a sample config file" do
-    filename = "spec/utils/config/init-test.yml"
+    filename = "config/init-test.yml"
     FileUtils.rm(filename) if File.exists?(filename)
     Init.exec(filename)
     File.exists?(filename).should eq true
--- spec/utils/config_spec.cr
+++ spec/utils/config_spec.cr
@@ -2,22 +2,22 @@ require "../spec_helper"
 
 describe Config do
   it "should load from a config file" do
-    config = Config.new "spec/utils/config/sample.yml"
+    config = Config.new "config/ldsync.yml"
     config.project.should eq "example-project"
     config.environment.should eq "example-environment"
     config.flags["example-flag"].should eq false
   end
 
   it "should support loading an empty config file for the init function" do
-    config = Config.new "spec/utils/config/sample.yml", "example-project", "example-environment"
+    config = Config.new "config/ldsync.yml", "example-project", "example-environment"
     config.project.should eq "example-project"
     config.environment.should eq "example-environment"
   end
 
   it "saves values to a yaml file" do
-    config = Config.new "spec/utils/config/sample.yml"
-    FileUtils.rm "spec/utils/config/sample.yml"
+    config = Config.new "config/ldsync.yml"
+    FileUtils.rm "config/ldsync.yml"
     config.save
-    File.exists?("spec/utils/config/sample.yml").should eq true
+    File.exists?("config/ldsync.yml").should eq true
   end
 end
